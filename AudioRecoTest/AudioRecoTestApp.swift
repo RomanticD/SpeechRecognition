a140b72989ee55fcb8c7d645785938675e324e7e
Junhua Di <96178954+RomanticD@users.noreply.github.com>
2024-09-11 18:01:01 +0800

    Initial Commit

---
+++ AudioRecoTest/AudioRecoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecoTestApp.swift
+//  AudioRecoTest
+//
+//  Created by Romantic D on 2024/9/11.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioRecoTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioRecoTest
+//
+//  Created by Romantic D on 2024/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
